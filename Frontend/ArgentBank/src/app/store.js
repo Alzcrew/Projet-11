b977d22e8476b970a9c1a41c6a0244156f8d5cb2
Léo <leo1.gully@gmail.com>
2023-12-01 11:39:07 +0100

    *

--- Frontend/ArgentBank/src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/counter/authSlice";
-export var store = configureStore({
-    reducer: {
-        auth: authReducer,
-    },
-});
